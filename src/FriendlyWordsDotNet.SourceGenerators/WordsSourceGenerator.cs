d2a6413fd09403071b851a2fc3df093d56f3e19d
LykaiosNZ <ben@mrflux.net.nz>
2021-04-20 11:23:38 +1200

    Initial Commit

---
+++ src/FriendlyWordsDotNet.SourceGenerators/WordsSourceGenerator.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+using System.IO;
+using System.Text;
+
+namespace FriendlyWordsDotNet
+{
+    [Generator]
+    public class WordsSourceGenerator : ISourceGenerator
+    {
+        public void Execute(GeneratorExecutionContext context)
+        {
+
+            var sb = new StringBuilder(@"
+using System;
+
+namespace FriendlyWordsDotNet
+{
+    public partial class FriendlyWords
+    {
+            ");
+            
+            foreach (var file in context.AdditionalFiles)
+            {
+                sb.AppendLine(GeneratePropertyFromFile(file));
+            }
+
+            sb.Append(@"
+    }
+}");
+
+            context.AddSource("FriendlyWordsGenerated.cs", SourceText.From(sb.ToString(), Encoding.UTF8));
+        }
+
+        public void Initialize(GeneratorInitializationContext context)
+        {
+        }
+
+        private string GeneratePropertyFromFile(AdditionalText file)
+        {
+            var fileName = Path.GetFileNameWithoutExtension(file.Path);
+
+            var fieldName = "_" + fileName.ToLowerInvariant();
+            var propertyName = System.Globalization.CultureInfo.InvariantCulture.TextInfo.ToTitleCase(fileName.ToLower());
+
+            var lines = file.GetText().Lines;
+
+            var sb = new StringBuilder($@"
+        private static readonly string[] {fieldName} = new[] 
+        {{
+");
+
+            foreach (var line in lines)
+            {
+                sb.AppendLine($"           \"{line}\",");
+            }
+
+            sb.Append($@"
+        }};
+
+        public static WordCollection {propertyName} {{get;}} = new WordCollection({fieldName});");
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ src/FriendlyWordsDotNet/FriendlyWords.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FriendlyWordsDotNet
+{
+    public partial class FriendlyWords
+    {
+    }
+
+    public class WordCollection : IReadOnlyCollection<string>
+    {
+        private readonly ILookup<int, string> _wordsByLength;
+
+        public WordCollection(IEnumerable<string> words)
+        {
+            _wordsByLength = words.ToLookup(w => w.Length);
+        }
+
+        public int Count => _wordsByLength.Count;
+
+        public IEnumerator<string> GetEnumerator() => _wordsByLength.SelectMany(w => w).GetEnumerator();
+
+        public IEnumerable<string> OfLength(int length) => _wordsByLength[length];
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
